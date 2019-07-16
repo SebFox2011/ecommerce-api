56fbd91b6590d56cc7098d1fa848f21d646a58dd
SebFox2011 <sebastien.philippe5@gmail.com>
2019-07-16 16:48:42 +0200

    Ajout du fichier .env pour gérer les variables d'environnement du server et de l'api

--- ecommerce-react/src/Component/ProductThumb.js
+++ ecommerce-react/src/Component/ProductThumb.js
@@ -2,9 +2,14 @@ import React, {Component} from 'react';
 
 class ProductThumb extends Component {
     render() {
+
+        const {product} = this.props;
+
         return (
             <div>
-                {this.props.product.name}
+                <img src={"http://127.0.0.1:8000/uploads/" + product.picture} alt="Image du produit"/>
+                {product.name}
+                <p>Prix: {product.price} €</p>
             </div>
         );
     }
