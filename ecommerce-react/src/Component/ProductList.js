a83ed90feb00b2e91a4d028ef30c3494bce249cf
SebFox2011 <sebastien.philippe5@gmail.com>
2019-07-16 16:19:21 +0200

    Connection du composant ProductList a l'api

--- ecommerce-react/src/Component/ProductList.js
+++ ecommerce-react/src/Component/ProductList.js
@@ -1,6 +1,20 @@
 import React, {Component} from 'react';
 
 class ProductList extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+          products:[]
+        };
+    }
+
+    //Appel à l'api
+    componentDidMount() {
+        fetch('http://127.0.0.1:8000/api/products')
+            .then(response => response.json())
+            .then(data => this.setState({products: data["hydra:member"]}))
+    }
+
     render() {
         return (
             <div className="Product-list">
